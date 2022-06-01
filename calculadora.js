2c16fd0eff5798fde16be04006aad85abba7b33f
Servin Maria <106326633+Mery347@users.noreply.github.com>
2022-06-01 19:25:46 -0300

    modificacion en calculadora con un switch

--- calculadora.js
+++ calculadora.js
@@ -3,5 +3,19 @@ let suma = require('./suma')
 
 let calculadora = {
     suma : suma(12,8)
+    resta : resta (4,2)
 
+}
+let num1 = +arg[4];
+let num2 = +arg [4]
+switch (argv[2]) {
+    case "suma" :
+        console.log(suma());
+        
+        break;
+    case "resta": 
+    console.log(resta());
+
+    default:
+        break;
 }
